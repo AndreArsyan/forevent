13299c2fc500b32e05f8edbffb2e5a7317460f38
Andre Arsyan <andre@andrearsyan.com>
2014-10-01 14:39:00 +0700

    add promo

--- index.php
+++ index.php
@@ -18,44 +18,60 @@
 	</div>
 	<div class="col-md-3"></div>
 </div>
-<div class="row" style="margin-top:20px">
+
+<div class="row" style="margin-top:20px;">
+	<h1 style="margin-bottom:20px;">Special Promotion</h1>
+	<div class="col-md-3">
+	<img src="img/promo1.jpg" alt="promo 1" style="width:180px;height:180px" class="img-rounded">
+	</div>
+	<div class="col-md-3">
+	<img src="img/promo4.png" alt="promo 4"  style="width:180px;height:180px"  class="img-rounded">
+	</div>
+	<div class="col-md-3">
+	<img src="img/promo2.gif" alt="promo 2"  style="width:180px;height:180px"  class="img-rounded">
+	</div><div class="col-md-3">
+	<img src="img/promo3.jpg" alt="promo 3"  style="width:180px;height:180px"  class="img-rounded">
+	</div>
 	
+	
+</div>
+
+<div class="row" style="margin-top:20px">
 	<div class="col-md-12">
 	<h1>Featured Product</h1>
 		<div id="mi-slider" class="mi-slider">
-					<ul>
-						<li><a href="#"><img src="images/1.jpg" alt="img01"><h4>Boots</h4></a></li>
-						<li><a href="#"><img src="images/2.jpg" alt="img02"><h4>Oxfords</h4></a></li>
-						<li><a href="#"><img src="images/3.jpg" alt="img03"><h4>Loafers</h4></a></li>
-						<li><a href="#"><img src="images/4.jpg" alt="img04"><h4>Sneakers</h4></a></li>
-					</ul>
-					<ul>
-						<li><a href="#"><img src="images/5.jpg" alt="img05"><h4>Belts</h4></a></li>
-						<li><a href="#"><img src="images/6.jpg" alt="img06"><h4>Hats &amp; Caps</h4></a></li>
-						<li><a href="#"><img src="images/7.jpg" alt="img07"><h4>Sunglasses</h4></a></li>
-						<li><a href="#"><img src="images/8.jpg" alt="img08"><h4>Scarves</h4></a></li>
-					</ul>
-					<ul>
-						<li><a href="#"><img src="images/9.jpg" alt="img09"><h4>Casual</h4></a></li>
-						<li><a href="#"><img src="images/10.jpg" alt="img10"><h4>Luxury</h4></a></li>
-						<li><a href="#"><img src="images/11.jpg" alt="img11"><h4>Sport</h4></a></li>
-					</ul>
-					<ul>
-						<li><a href="#"><img src="images/12.jpg" alt="img12"><h4>Carry-Ons</h4></a></li>
-						<li><a href="#"><img src="images/13.jpg" alt="img13"><h4>Duffel Bags</h4></a></li>
-						<li><a href="#"><img src="images/14.jpg" alt="img14"><h4>Laptop Bags</h4></a></li>
-						<li><a href="#"><img src="images/15.jpg" alt="img15"><h4>Briefcases</h4></a></li>
-					</ul>
-					<nav>
-						<a href="#">Shoes</a>
-						<a href="#">Accessories</a>
-						<a href="#">Watches</a>
-						<a href="#">Bags</a>
-					</nav>
-				</div>
+			<ul>
+				<li><a href="#"><img src="images/1.jpg" alt="img01"><h4>Boots</h4></a></li>
+				<li><a href="#"><img src="images/2.jpg" alt="img02"><h4>Oxfords</h4></a></li>
+				<li><a href="#"><img src="images/3.jpg" alt="img03"><h4>Loafers</h4></a></li>
+				<li><a href="#"><img src="images/4.jpg" alt="img04"><h4>Sneakers</h4></a></li>
+			</ul>
+			<ul>
+				<li><a href="#"><img src="images/5.jpg" alt="img05"><h4>Belts</h4></a></li>
+				<li><a href="#"><img src="images/6.jpg" alt="img06"><h4>Hats &amp; Caps</h4></a></li>
+				<li><a href="#"><img src="images/7.jpg" alt="img07"><h4>Sunglasses</h4></a></li>
+				<li><a href="#"><img src="images/8.jpg" alt="img08"><h4>Scarves</h4></a></li>
+			</ul>
+			<ul>
+				<li><a href="#"><img src="images/9.jpg" alt="img09"><h4>Casual</h4></a></li>
+				<li><a href="#"><img src="images/10.jpg" alt="img10"><h4>Luxury</h4></a></li>
+				<li><a href="#"><img src="images/11.jpg" alt="img11"><h4>Sport</h4></a></li>
+			</ul>
+			<ul>
+				<li><a href="#"><img src="images/12.jpg" alt="img12"><h4>Carry-Ons</h4></a></li>
+				<li><a href="#"><img src="images/13.jpg" alt="img13"><h4>Duffel Bags</h4></a></li>
+				<li><a href="#"><img src="images/14.jpg" alt="img14"><h4>Laptop Bags</h4></a></li>
+				<li><a href="#"><img src="images/15.jpg" alt="img15"><h4>Briefcases</h4></a></li>
+			</ul>
+			<nav>
+				<a href="#">Shoes</a>
+				<a href="#">Accessories</a>
+				<a href="#">Watches</a>
+				<a href="#">Bags</a>
+			</nav>
+		</div>
 	</div>
 </div>
-
 
 
 
